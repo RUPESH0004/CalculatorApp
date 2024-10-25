1273adde172a329fa4640e9725f8ef40fbaa4948
RUPESH0004 <152394146+RUPESH0004@users.noreply.github.com>
2024-10-25 17:13:31 +0530

    Completed the js first project with the logic and important concept like the try catch or throw functions used loops and evenlistner For dom manipulations

--- Logic.js
+++ Logic.js
@@ -1,11 +1,39 @@
-const display = document.querySelector('.display-input');
-const btn = document. querySelectorAll('.buttons');
-const clearbtn = document.querySelector('.clear');
-const cutbtn = document.querySelector('.cut');
-const equalbtn = document.querySelector('.button equals');
+const display = document.querySelector(".display-input");
+const btn = document.querySelectorAll(".button");
+const clearbtn = document.querySelector(".clear");
+const cutbtn = document.querySelector(".cut");
+const equalbtn = document.querySelector(".equals");
 let currentInput = "";
-function updateDisplay()
-{
-    display.value = currentInput;
+btn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (currentInput === "0" || currentInput === "Error") {
+      currentInput = "";
+    }
+    currentInput += e.target.innerHTML;
+    updateDisplay();
+  });
+});
+
+function updateDisplay() {
+  display.value = currentInput;
 }
-updateDisplay();
+clearbtn.addEventListener("click", () => {
+  currentInput = "";
+  updateDisplay();
+});
+equalbtn.addEventListener("click", () => {
+  try {
+    const result = eval(currentInput);
+    currentInput = result;
+    if (result === Infinity || result === isNaN(result)) {
+      throw new Error("Invalide calculation");
+    }
+  } catch (Error) {
+    currentInput = "Error";
+  }
+  updateDisplay();
+});
+cutbtn.addEventListener("click", () => {
+  currentInput = currentInput.slice(0, -1);
+  updateDisplay();
+});
